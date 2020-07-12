868bbf39e725087a059e7eae1c4ec0bd1c80f843
CA <colbyackerman@gmail.com>
2020-07-12 16:33:07 -0500

    add test for printing string arrays

--- test.c
+++ test.c
@@ -1,46 +1,76 @@
 #include "pretty_printer.h"
 
-static void initStringArray(char* strArray[], size_t size, char* initVal) {
-    int i = 0;
-    char testString[MAX_STRING_LENGTH];
+// Tests
+static void testPrintAllStringElementsOfArray();
 
-    for(i = 0; i < size; ++i) {
-        sprintf(testString, "hello %d", i);
-        strArray[i] = testString;
-        printf("testString = \"%s\"\nstrArray[%d] = \"%s\"\naddr = %p\n\n", testString, i, strArray[i], &strArray[i]);
-    }
-}
+// Utility Functions
+static void printTestHeader(const char* testName);
+static void incrementalStringArrayInit(char* stringArray[], const size_t size);
+static void cleanupStringArray(char* stringArray[], const size_t size);
 
-static void printStringArray(char* strArray[], size_t size) {
-    int i;
-    for(i = 0; i < size; ++i) {
-        printf("%d = %s\n", i, strArray[i]);
-    }
-}
+// ---
 
 int main() {
-    
-    #define SIZE_3 3
 
-    char* stringArray_size3[SIZE_3];
-
-    //initialize string array elements (same as initStringArray() just pulled out for debugging purposes)
-    int i;
-    for(i = 0; i < SIZE_3; ++i) {
-        char testString[MAX_STRING_LENGTH];
-        sprintf(testString, "hello %d", i);
-        stringArray_size3[i] = testString;
-        printf("testString = \"%s\"\nstrArray[%d] = \"%s\"\naddr = %p\n\n", testString, i, stringArray_size3[i], &stringArray_size3[i]);
-    }
-
-    //Debug print
-    printf (
-        "%p = %s\n%p = %s\n%p = %s\n%p = %s\n", 
-        &stringArray_size3, *stringArray_size3,     // <-- sanity check
-        &stringArray_size3[0], stringArray_size3[0],
-        &stringArray_size3[1], stringArray_size3[1],
-        &stringArray_size3[2], stringArray_size3[2]
-    );
+    // Run Tests
+    testPrintAllStringElementsOfArray();
 
     return 0;
+}
+
+// --- Test Definitions ---
+
+static void testPrintAllStringElementsOfArray() {
+
+    printTestHeader(__func__);
+
+    const size_t SIZE_0  = 0;
+    const size_t SIZE_1  = 1;
+    const size_t SIZE_3  = 3;
+    const size_t SIZE_10 = 10;
+
+    char* stringArray_size0   [SIZE_0];
+    char* stringArray_size1   [SIZE_1];
+    char* stringArray_size3   [SIZE_3];
+    char* stringArray_size10  [SIZE_10];
+
+    // Init
+    incrementalStringArrayInit(stringArray_size0, SIZE_0);
+    incrementalStringArrayInit(stringArray_size1, SIZE_1);
+    incrementalStringArrayInit(stringArray_size3, SIZE_3);
+    incrementalStringArrayInit(stringArray_size10, SIZE_10);
+
+    // Print
+    printArray(stringArray_size0, SIZE_0, STRING);
+    printArray(stringArray_size1, SIZE_1, STRING);
+    printArray(stringArray_size3, SIZE_3, STRING);
+    printArray(stringArray_size10, SIZE_10, STRING);
+
+    // Cleanup
+    cleanupStringArray(stringArray_size0, SIZE_0);
+    cleanupStringArray(stringArray_size1, SIZE_1);
+    cleanupStringArray(stringArray_size3, SIZE_3);
+    cleanupStringArray(stringArray_size10, SIZE_10);
+}
+
+// --- Utility ---
+
+static void printTestHeader(const char* testName) {
+    printf("--- %s ---\n\n", testName);
+}
+
+// Initialize elements of the string array incrementally for easier debugging.
+static void incrementalStringArrayInit(char* stringArray[], const size_t size) {
+    int i;
+    for(i = 0; i < size; ++i) {
+        stringArray[i] = malloc(sizeof(char*) * MAX_STRING_LENGTH);
+        sprintf(stringArray[i], "string #%.2d", i);
+    }
+}
+
+static void cleanupStringArray(char* stringArray[], const size_t size) {
+    int i;
+    for(i = 0; i < size; ++i) {
+        free(stringArray[i]);
+    }
 }
